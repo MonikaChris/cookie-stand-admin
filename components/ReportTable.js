2f52158cc4ea5bbc2d8c67ede614483148288ea3
MonikaChris <monidavies@gmail.com>
2023-02-13 06:28:10 -0700

    Styles table

--- components/ReportTable.js
+++ components/ReportTable.js
@@ -31,34 +31,32 @@ export default function ReportTable(props) {
         return (
             <table className="w-1/2 mx-auto my-4 border">
                 <thead>
-                    <tr>
-                        <th className="border border-black">Location</th>
+                    <tr className="text-center bg-green-400">
+                        <th>Location</th>
                         {hours.map(item => (
                             <th key={item}>{item}</th>
                         ))}
-                        <th className="border border-black">Totals</th>
+                        <th className="">Totals</th>
                     </tr>
                 </thead>
                 <tbody>
                     {cookieStandList.map(stand => (
-                        (<tr key={stand.id}>
-                            <td>{stand.location}</td>
+                        (<tr className="even:bg-green-100 odd:bg-green-300" key={stand.id}>
+                            <td className="border border-black">{stand.location}</td>
                             {stand.hourly_sales.map( hour => (
-                                <td key={hour}>{hour}</td>
+                                <td className="border border-black" key={hour}>{hour}</td>
                             ))}
-                            <td>{getRowTotal(stand.hourly_sales)}</td>
+                            <td className="border border-black">{getRowTotal(stand.hourly_sales)}</td>
                         </tr>)
                     ))}
 
-                    <tr>
-                        <td>Totals</td>
+                    <tr className="text-center font-bold bg-green-400">
+                        <td className="border border-black">Totals</td>
                         {hour_idx.map(idx => (
-                            <td key={idx}>{getColTotal(idx)}</td>
+                            <td className="border border-black" key={idx}>{getColTotal(idx)}</td>
                         ))}
-                        <td>{total_of_totals}</td>
+                        <td className="border border-black">{total_of_totals}</td>
                     </tr>
-
-
                     
                 </tbody>
             </table>
